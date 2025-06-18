4909fd7819bbe4d05aab57f2d121f30f7fd4985a
dannyednut <dannyednut88@gmail.com>
2025-06-19 00:59:39 +0100

    new model created

--- Assignments/Beginner/Assignment-5/project/Scripts/project/advenza/models.py
+++ Assignments/Beginner/Assignment-5/project/Scripts/project/advenza/models.py
@@ -5,3 +5,8 @@ class advenza_user(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     gender = models.CharField(max_length=10)
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    gender = models.CharField(max_length=10)
