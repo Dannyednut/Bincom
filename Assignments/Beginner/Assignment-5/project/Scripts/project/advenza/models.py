3f1b1da610253c596b590c6ecd2326b1727f5786
dannyednut <dannyednut88@gmail.com>
2025-06-19 00:30:49 +0100

    database table modifed

--- Assignments/Beginner/Assignment-5/project/Scripts/project/advenza/models.py
+++ Assignments/Beginner/Assignment-5/project/Scripts/project/advenza/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class advenza_user(models.Model):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     gender = models.CharField(max_length=10)
